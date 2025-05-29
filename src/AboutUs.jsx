a08f786e2139482141f6174a737f17f1b160e66f
Demola <adedojadaniel@gmail.com>
2025-05-29 13:21:13 +0100

    1

--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -7,25 +7,18 @@ function AboutUs() {
   return (
     <div className="about-us-container">
       {/* <h1 className="about-us-heading">About Us</h1> */}
-      <p className="about-us-description">Welcome to Paradise Nursery, where green meets serenity!</p>
+      <p className="about-us-description">Welcome To Pronto Microsystems Technologies Limited, where Communication is made Easy!</p>
       <p className="about-us-content">
-        At Paradise Nursery, we are passionate about bringing nature closer to you. Our mission is to provide a wide range of 
-        high-quality plants that not only enhance the beauty of your surroundings but also contribute to a healthier and 
-        more sustainable lifestyle. From air-purifying plants to aromatic fragrant ones, we have something for every 
-        plant enthusiast.
+        Pronto Microsystems Technologies Limited is a Nigerian Army own registered company that was established on 22 April and situated in Mile 2 Signal Barracks.We are a company that provides seamless interoperable voice and data tranmission solutions using the DMR Standard. Our hybrid brand of radios offers efficient voice communication on digital and analog channels, and can be programmed to work with any DMR Repeater.
       </p>
       {/* <p className="plant_logo_left"><img src="https://p1.hiclipart.com/preview/922/979/640/green-leaf-logo-emoji-seedling-emoticon-sticker-plant-plant-stem-flower-png-clipart-thumbnail.jpg" height='50px' width='50px' alt="" /></p> */}
       <p className="about-us-content">
-        Our team of experts is dedicated to ensuring that each plant meets our strict standards of quality and care. 
-        Whether you're a seasoned gardener or just starting your green journey, we're here to support you every step of 
-        the way. Feel free to explore our collection, ask questions, and let us help you find the perfect plant for your 
-        home or office.
+        We are committed to delivering products of not only the highest quality and reliability but also ones that meet the unique requirements of our customers. Our state-of-art factory employ innovative processes that guarantee high quality products.
       </p>
       {/* <p className="plant_logo_right"><img src="https://p1.hiclipart.com/preview/922/979/640/green-leaf-logo-emoji-seedling-emoticon-sticker-plant-plant-stem-flower-png-clipart-thumbnail.jpg" height='50px' width='50px' alt="" /></p> */}
 
       <p className="about-us-content">
-        Join us in our mission to create a greener, healthier world. Visit Paradise Nursery today and experience the 
-        beauty of nature right at your doorstep.
+        The confidence of our customers in the quality services provided by PMTL must be continuously reinforced by our dedication to maintain and seek methods of improving every element of our operations that contributes to total customer satisfaction.
       </p>
     </div>
   );
--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -222,8 +222,8 @@ function ProductList() {
     ];
 
     const styleObj = {
-        backgroundColor: '#4CAF50',
-        color: '#fff!important',
+        backgroundColor: '#26272b',
+        color: '#ff0000!important',
         padding: '15px',
         display: 'flex',
         justifyContent: 'space-between',
@@ -275,17 +275,17 @@ function ProductList() {
             <div className="navbar" style={styleObj}>
                 <div className="tag">
                     <div className="luxury">
-                        <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt="" />
+                        <img src="https://pmtl.ng/assets/pronto-df59e8b7.webp" alt="" />
                         <a href="/" style={{ textDecoration: 'none' }}>
                             <div>
-                                <h3 style={{ color: 'white' }}>Paradise Nursery</h3>
-                                <i style={{ color: 'white' }}>Where Green Meets Serenity</i>
+                                <h3 style={{ color: 'white' }}>PMTL</h3>
+                                <i style={{ color: 'white' }}>Where Communication is made Easy</i>
                             </div>
                         </a>
                     </div>
                 </div>
                 <div style={styleObjUl}>
-                    <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
+                    <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>PRODUCTS</a></div>
                     <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path><text x="90" y="155" font-family="Verdana" font-size="90" fill="white">{cart.numOfItems}</text></svg></h1></a></div>
                 </div>
             </div>

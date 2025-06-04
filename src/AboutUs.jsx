b8c7b7fe126a1374f5ae6cf96021cb77f9816df4
Demola <adedojadaniel@gmail.com>
2025-06-04 09:56:40 +0100

    1

--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -9,13 +9,7 @@ function AboutUs() {
       <h1 className="about-us-heading">About Us</h1>
       <p className="about-us-description">Welcome To Pronto Microsystems Technologies Limited, where Communication is made Easy!</p>
       <p className="about-us-content">
-        Pronto Microsystems Technologies Limited is a Nigerian Army own registered company that was established on 22 April and situated in Mile 2 Signal Barracks.We are a company that provides seamless interoperable voice and data tranmission solutions using the DMR Standard. Our hybrid brand of radios offers efficient voice communication on digital and analog channels, and can be programmed to work with any DMR Repeater.
-      </p>
-      <p className="about-us-content">
         We are committed to delivering products of not only the highest quality and reliability but also ones that meet the unique requirements of our customers. Our state-of-art factory employ innovative processes that guarantee high quality products.
-      </p>
-      <p className="about-us-content">
-        The confidence of our customers in the quality services provided by PMTL must be continuously reinforced by our dedication to maintain and seek methods of improving every element of our operations that contributes to total customer satisfaction.
       </p>
     </div>
   );
